63d61f5fa6fddbf8346f79841d6d3eb0d09d1fa8
Danilo Nunes <danilo8nunes@gmail.com>
2020-11-26 11:27:00 -0300

    Projeto removido

--- README.md
+++
@@ -1,2 +0,0 @@
-# curso_github
-Curso Upinside
--- assets/views/404.php
+++
@@ -1,7 +0,0 @@
-<div class='container p-2 mx-auto d-flex justify-content-center align-items-center'>
-    <span class="mr-2">
-        <h1 class="text-center display-2">404</h1>
-        <h5 class="text-center">NOT FOUND!</h5>
-    </span>
-    <img class="w-50" src="<?=url();?>assets/image/error-404.png">
-</span>
